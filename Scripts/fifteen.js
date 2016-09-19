000f648431fe3015d522e79d888a0d8dbd6a8d4e
Matthew H. Irby <irby94@uga.edu>
2016-09-18 22:36:59 -0400

    Modified CSS file

--- Scripts/fifteen.js
+++ Scripts/fifteen.js
@@ -1,4 +1,7 @@
-//  Group Name: The Average Joes
+/*  Group Name: The Average Joes
 //  Group Members: Bruce Li (Leader), Matt Irby, Wesley Nguyen, Zachary Gay
 //  File Name: fifteen.js
 //  Purpose: This is the javascript file fifteen.html's puzzle piece movement.
+*/
+
+
